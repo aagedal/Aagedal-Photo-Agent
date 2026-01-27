5279ad7808e24d2efbbfc95fe17ac51eea72927e
Truls Aagedal <5827165+aagedal@users.noreply.github.com>
2026-01-27 10:59:23 +0100

    Initial Commit

---
+++ Aagedal Photo Agent/Aagedal_Photo_AgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aagedal_Photo_AgentApp.swift
+//  Aagedal Photo Agent
+//
+//  Created by Truls Aagedal on 27/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct Aagedal_Photo_AgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
